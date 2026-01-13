acb8b9d1f2e271523237c2865810f20f46aa709a
Tanekam Kenfack <Baurel.Tanekam@medien-systempartner.de>
2026-01-14 00:59:28 +0100

    Datenklasse für die Simulation Konfig hinzugefügt

---
+++ src/main/java/view/SimulationConfig.java
@@ -0,0 +1,14 @@
+package view;
+
+/**
+ * Datenklasse für die Simulations-Konfiguration.
+ */
+public class SimulationConfig {
+    public boolean istKugelstosspendel;
+    public double masse;
+    public double radius;
+    public double startWinkelGrad;
+    public int anzahlKugeln;
+    public int anzahlAusgelenkt;
+    public double restitutionskoeffizient;
+}
